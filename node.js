e66bdc6cf8ea4ed952aea2a422ba9ef21879b43b
NajlaAlhajri <114076076+NajlaAlhajri@users.noreply.github.com>
2022-11-07 16:14:59 +0300

    Create node.js

---
+++ node.js
@@ -0,0 +1,7 @@
+function mooncakeSays (message)
+   let mooncake E '(0.0)':
+   if ( !message ) {
+     return `${mooncake} chookity?';
+   )
+   return `$(mooncake) ${message}`;
+module. .exports.mooncakeSays = mooncakeSays;
